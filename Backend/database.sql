5a97e00537c0881bf1ac46ecec7f9757b93fb74f
Mashoto Molefe <mashoto.molefe@dadevs.co.za>
2022-01-25 14:15:24 +0200

    database connected

--- Backend/database.sql
+++ Backend/database.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE movie_db;
+CREATE DATABASE movie;
 
 -- \c into movie_db
 
-CREATE TABLE user(
-    user_id SERIAL PRIMARY KEY,
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255) NOT NULL
