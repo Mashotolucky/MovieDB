5fddcd9358d6117d857c90fe58fdfa2bced59d6f
Mashoto Molefe <mashoto.molefe@dadevs.co.za>
2022-01-26 16:25:34 +0200

    movie items api committed

--- frontend/src/app/components/movies-list-items/movies-list-items.component.ts
+++ frontend/src/app/components/movies-list-items/movies-list-items.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieServiceService } from 'src/app/services/movie-service.service';
+// import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-movies-list-items',
@@ -9,20 +10,52 @@ import { MovieServiceService } from 'src/app/services/movie-service.service';
 export class MoviesListItemsComponent implements OnInit {
 
   rating = 0;
+  movieArr: any = [];
+  results: any = [];
+  title: any;
+  pic: any;
+
+
   constructor(private movieApi: MovieServiceService) { 
     
   }
 
   ngOnInit(): void {
-
+    this.getPupolarMovies();
+    this.getMovie();
+    //this.getPop();
   }
 
   ratings(){
     return this.rating = this.rating + 1;
   }
 
+  // getPop(): void{
+  //   this.movieApi.popularMovies().subscribe((res: any)=>{
+  //     //this.movieArr = res;
+  //     console.log("the popular obj: "+res)
+  //   })
+  // }
+
   getPupolarMovies(): void{
-    this.movieApi.popularMovies
+    this.movieApi.popularMovies()
+    .subscribe((res: any) => {
+      this.movieArr = res.results; 
+      console.log(this.movieArr);
+      //console.log(this.movieArr[0].title);
+      
+    },err =>{
+      console.log(err);
+      
+    })
   }
 
+  getMovie(): void{
+    this.title = this.movieArr[0].title;
+
+    // this.pic = this.movieArr.results.backdrop_path;
+    console.log(this.title);
+    // console.log(this.pic);
+    
+  }
 }
